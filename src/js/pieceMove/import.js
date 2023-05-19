7615ede989adbeb7fd7c524f4fbee86088ef7af0
EnzooKms <ekemmas@edenschool.fr>
2023-05-19 18:06:13 +0200

    export bishop func

--- src/js/pieceMove/import.js
+++ src/js/pieceMove/import.js
@@ -2,10 +2,12 @@ import pawn from "./pawn";
 import king from "./king";
 import queen from "./queen";
 import rook from "./rook";
+import bishop from "./bishop";
 
 export {
     pawn,
     king,
     queen,
-    rook
+    rook,
+    bishop
 }
