09909e1eae01cc6623ca77bb3657caf01c5dc0fd
EnzooKms <ekemmas@edenschool.fr>
2023-05-23 17:00:16 +0200

    func check if isCheckMate

---
+++ src/js/isCheckMate.js
@@ -0,0 +1,42 @@
+import { pawn, king, queen, rook, bishop, knight } from "./pieceMove/import"
+
+export default function isCheckMate(states, turn) {
+
+    const allPieces = states.filter(el => el.piece && el.piece.endsWith(turn))
+    let haveMove = false
+
+    for (const piece of allPieces) {
+
+        const dot = '<div class="dot"></div>'
+
+
+        if (piece.piece.startsWith('pawn')) {
+            if (pawn(piece, states, turn, dot)) haveMove = true
+        }
+
+        if (piece.piece.startsWith('king')) {
+            if (king(piece, states, turn, dot)) haveMove = true
+        }
+
+        if (piece.piece.startsWith('queen')) {
+            if (queen(piece, states, turn, dot)) haveMove = true
+        }
+
+        if (piece.piece.startsWith('rook')) {
+            if (rook(piece, states, turn, dot)) haveMove = true
+        }
+
+        if (piece.piece.startsWith('bishop')) {
+            if (bishop(piece, states, turn, dot)) haveMove = true
+        }
+
+        if (piece.piece.startsWith('knight')) {
+            if (knight(piece, states, turn, dot)) haveMove = true
+        }
+    }
+
+    if (!haveMove) {
+        alert('checkmate')
+    }
+
+}
